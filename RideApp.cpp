80b5aa0fb5b820e9a7dedb8746a9424590360065
Damian <damianram3504@gmail.com>
2025-09-29 04:20:17 +0800

    Add name and location inputs

--- RideApp.cpp
+++ RideApp.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 int main() 
 {
-   
+    string name, pickupLocation, dropoffLocation;
+    
+    do {
+        cin.ignore();
+
+        //input passenger name
+        cout << "Enter your name: ";
+        getline(cin, name);
+
+        //input pickup location
+        cout << "Enter Pickup Location: ";
+        getline(cin, pickupLocation);
+
+        //input drop-off location
+        cout << "Enter Drop-off Location: ";
+        getline(cin, dropoffLocation);
+
 }
 
